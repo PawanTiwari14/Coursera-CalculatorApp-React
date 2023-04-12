f4e193f2f205117c5c46fc4460d9c21bb822e9ab
Pawan Tiwari <pawan.pt.149@gmail.com>
2023-04-12 23:28:42 +0530

    added logic

--- App.js
+++ App.js
@@ -1,34 +1,87 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [num1, setNum1] = useState();
+  const [num2, setNum2] = useState();
+  const [result, setResult] = useState("Your Result");
+
+  const handleNum1Change = (event) => {
+    setNum1(Number(event.target.value));
+  };
+
+  const handleNum2Change = (event) => {
+    setNum2(Number(event.target.value));
+  };
+
+  const handleAdd = () => {
+    setResult(num1 + num2);
+  };
+
+  const handleSubtract = () => {
+    setResult(num1 - num2);
+  };
+
+  const handleMultiply = () => {
+    setResult(num1 * num2);
+  };
+
+  const handleDivide = () => {
+    setResult(num1 / num2);
+  };
+
+  const handleResetInput = () => {
+    setNum1(0);
+    setNum2(0);
+  };
+
+  const handleResetResult = () => {
+    setResult(0);
+  };
+
   return (
     <div className="App container-lg">
       <h1 className="m-3">Simplest Working Calculator App</h1>
       <input
-        name="text"
+        type="number"
+        name="num1"
         className="input m-3"
-        pattern="\d+"
+        value={num1}
+        onChange={handleNum1Change}
         placeholder="Input Your First Number to Calculate"
       ></input>
-      <span style={{ fontSize: "28px" }}>+</span>
       <input
-        name="text"
+        type="number"
+        name="num2"
         className="input m-3"
-        pattern="\d+"
+        value={num2}
+        onChange={handleNum2Change}
         placeholder="Input Your Second Number to Calculate"
       ></input>
       <div className="d-flex justify-content-center m-3">
-        <button className="p-2 m-1 op">Add</button>
-        <button className="p-2 m-1 op">Subtract</button>
-        <button className="p-2 m-1 op">Multiply</button>
-        <button className="p-2 m-1 op">Divide</button>
+        <p className="input m-3">{result}</p>
       </div>
       <div className="d-flex justify-content-center m-3">
-        <button className="reset p-2 m-1">Reset Input</button>
-        <button className="reset p-2 m-1">Reset Result</button>
+        <button className="p-2 m-1 op" onClick={handleAdd} name="arop">
+          Add
+        </button>
+        <button className="p-2 m-1 op" onClick={handleSubtract}>
+          Subtract
+        </button>
+        <button className="p-2 m-1 op" onClick={handleMultiply}>
+          Multiply
+        </button>
+        <button className="p-2 m-1 op" onClick={handleDivide}>
+          Divide
+        </button>
       </div>
       <div className="d-flex justify-content-center m-3">
-        <button className="equal">Equal = </button>
+        <button className="reset p-2 m-1" onClick={handleResetInput}>
+          Reset Input
+        </button>
+        <button className="reset p-2 m-1" onClick={handleResetResult}>
+          Reset Result
+        </button>
       </div>
     </div>
   );
